ed58835ae4f9ed8c6e30613f5d7e29100a603702
Edu Ubuntu <eduerondu@gmail.com>
2012-11-21 10:52:00 -0600

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bizcast::Application.config.secret_token = '37c223b5d5c5a164cee028ca9d842ee8f347c25eca449cf95bd579ff4f801c547d4f72012aab6cead6cea87a62dec219ca37c11ba0f66b0f9393bdf00fbddc4a'
